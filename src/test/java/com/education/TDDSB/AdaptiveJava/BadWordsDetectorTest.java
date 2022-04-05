91edf73b5591484f40685454fcc2a303e611d20a
Vladyslav Buhaiets <vladyslav.buhaiets@eleks.com>
2022-04-05 19:00:12 +0300

    Fix imports

--- src/test/java/com/education/TDDSB/AdaptiveJava/BadWordsDetectorTest.java
+++ src/test/java/com/education/TDDSB/AdaptiveJava/BadWordsDetectorTest.java
@@ -1,24 +1,23 @@
 package com.education.TDDSB.AdaptiveJava;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BadWordsDetectorTest {
     private final BadWordsDetector detector = new BadWordsDetector();
 
     @Test
-    void detectBadWordsIsSuccess(){
+    void detectBadWordsIsSuccess() {
         String text = "Some text to verify if text have fuck or shit in whole article) fuck shit fuck";
         Stream<String> expectedStream = Stream.of("fuck", "shit");
-        Stream<String> actualBadWords = detector.createBadWordsDetectingStream(text, List.of("fuck","shit","damn","hoe"));
+        Stream<String> actualBadWords = detector.createBadWordsDetectingStream(text, List.of("fuck", "shit", "damn", "hoe"));
         List<String> expected = expectedStream.collect(Collectors.toList());
         List<String> actual = actualBadWords.collect(Collectors.toList());
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 }
